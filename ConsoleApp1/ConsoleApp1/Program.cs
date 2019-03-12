1220353865084ac97d440eb876d695fe8a831551
30020332daniilblair <orthodoxgain@outlook.com>
2019-03-12 21:22:25 +1300

    comment

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Grade Finder 101");
+            Console.WriteLine("Please enter your mark. A whole number out of 100");
+            float mark = float.Parse("Console.Readline");
+            string message = "";
+
+            if (mark >= 89 && mark <= 100)
+                message = ("Very good thats a A+");
+            else if (mark >= 79 && mark <= 90)
+                message = ("Nice. Your mark equals to an A");
+            else if (mark >= 69 && <= 80)
+
+
+        }
+    }
+}
