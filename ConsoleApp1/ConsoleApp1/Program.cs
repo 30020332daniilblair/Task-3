219af016c53d23e5cfefcf72074443fbd80abac7
Daniil Blair <30020332@student.toiohomai.ac.nz>
2019-03-13 09:28:18 +1300

    comment

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -19,7 +19,28 @@ namespace ConsoleApp1
                 message = ("Very good thats a A+");
             else if (mark >= 79 && mark <= 90)
                 message = ("Nice. Your mark equals to an A");
-            else if (mark >= 69 && <= 80)
+            else if (mark >= 69 && mark <= 80)
+                message = ("Good job. You have achieved a B+");
+            else if (mark >= 59 && mark <= 70)
+                message = ("Keep it up. You have scored a B");
+            else if (mark >= 49 && mark <= 60)
+                message = ("You have scored a C");
+            else if (mark >= 39 && mark <= 50)
+                message = ("You score has earnt you a D. More study next time");
+            else if (mark >= 0 && mark <= 40)
+                message = ("F.Not good. Better luck next time");
+            else
+                message = ("You have enetered an incorrect key");
+            Console.WriteLine(message);
+            Console.ReadLine();
+
+        
+
+
+
+
+
+
 
 
         }
